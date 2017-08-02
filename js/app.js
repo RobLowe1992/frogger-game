ee37f5a630e48c5615f039a484b8b4ff05b9fd11
Robert Lowe <RobLowe1992@Yahoo.com>
2017-08-02 02:53:40 -0400

    Updated Frogger Game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt
+
+    if (this.x >= 505) {
+        this.x = 0;
+    };
+    checkCollision(this);
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,17 +33,108 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+Player.prototype.handleInput = function(key) {
+    if (key == 'left') {
+        player.x -= player.speed;
+    }
+    if (key == 'up') {
+        player.y -= player.speed - 20;
+    }
+    if (key == 'right') {
+        player.x += player.speed;
+    }
+    if (key == 'down') {
+        player.y += player.speed - 20;
+    }
+
+    console.log('User Input: ' + key);
+};
+
+
+var checkCollision = function(anEnemy) {
+    if (player.y + 130 >= anEnemy.y + 90
+        && player.x + 25 <= anEnemy.x + 90
+        && player.y + 70 <= anEnemy.y + 135
+        && player.x + 80 >= anEnemy.x + 10) {
+            console.log('Better luck next time!');
+            player.x = 202.5;
+            player.y = 383;
+            score = 0;
+            gameLevel = 1;
+            console.log('current score: '+ score+ ', current level: '+ gameLevel);
+            updateDifficulty(score);
+    }
+    if (player.y + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        console.log('Victory!');
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+
+        score += 1;
+        gameLevel += 1;
+        console.log('current score: '+ score+ ', current level: '+ gameLevel);
+        updateDifficulty(score);
+        $('#scoreDiv').text('Score: '+score+', Level: '+gameLevel+'');
+    }
+    if (player.y > 383) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+};
+
+var updateDifficulty = function(numEnemies) {
+    // remove all previous enemies on canvas
+    allEnemies.length = 0;
+
+    // load new set of enemies
+    for (var i = 0; i <= numEnemies; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+        allEnemies.push(enemy);
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [],
+    player = new Player(202.5, 383, 50),
+    score = 0,
+    gameLevel = 1,
+    enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256),
+    scoreDiv = $('body:last').append('<div><h2 id="scoreDiv">Score: '+score+', Level: '+gameLevel+'</h2></div>');
+
+allEnemies.push(enemy);
+
+
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -43,4 +143,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    console.log(allowedKeys[e.keyCode]);
 });
